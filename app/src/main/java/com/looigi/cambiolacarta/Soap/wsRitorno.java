9d59ab751d4ef53f316d0f545191d71a74a15561
Luigi Pecce <looigi@gmail.com>
2019-06-05 16:01:57 +0200

    Sincronizzazione immagine in rete

--- app/src/main/java/com/looigi/cambiolacarta/Soap/wsRitorno.java
+++ app/src/main/java/com/looigi/cambiolacarta/Soap/wsRitorno.java
@@ -8,6 +8,8 @@ import com.looigi.cambiolacarta.SharedObjects;
 import com.looigi.cambiolacarta.Utility;
 import com.looigi.cambiolacarta.VariabiliGlobali;
 
+import java.util.Random;
+
 public class wsRitorno {
     private Runnable runRiga;
     private Handler hSelezionaRiga;
@@ -28,7 +30,32 @@ public class wsRitorno {
                     runRiga = null;
 
                     String[] r = Ritorno.split(";");
-                    int numero = Integer.parseInt(r[0]);
+                    // int numero = Integer.parseInt(r[0]);
+
+                    int numero = -1;
+
+                    String i = r[1].replace("\\", "/").toUpperCase().trim();
+                    int conta = 0;
+                    for (String s : SharedObjects.getInstance().getListaImmagini()){
+                        if (s.toUpperCase().trim().contains(i)) {
+                            numero=conta;
+                            break;
+                        }
+                        conta++;
+                    }
+                    if (numero == -1) {
+                        numero = Integer.parseInt(r[0]);
+                        if (numero>SharedObjects.getInstance().getListaImmagini().size()-1){
+                            Random rr;
+                            rr=new Random();
+                            int i1=rr.nextInt(SharedObjects.getInstance().getListaImmagini().size());
+                            i1--;
+                            if (i1<0) {
+                                i1=0;
+                            }
+                            numero=i1;
+                        }
+                    }
 
                     Utility u = new Utility();
                     u.CambiaImmagine(true, numero);
